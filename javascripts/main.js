9344c7c05e13b1304c05d9866aa76cd9099ba65c
Don Wilson <robert.don.wilson@gmail.com>
2012-09-18 10:35:47 -0600

    Show/hide UserVoice from the URL

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,17 @@
-console.log('This would be the main JS file.');
+$(function() {
+
+  var handleHash = function(hash) {
+    console.log('handling: ' + hash);
+    if (hash.indexOf('feedback') === -1) {
+      UserVoice.hidePopupWidget();
+    } else {
+      UserVoice.showPopupWidget();
+    }
+  }
+
+  $(window).bind('hashchange', function() {
+    handleHash(window.location.hash);
+  });
+  
+  handleHash(window.location.hash);
+});
