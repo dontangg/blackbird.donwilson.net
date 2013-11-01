37d065fbaac679a053dc02ee088e737f9b5e3efc
Don Wilson <robert.don.wilson@gmail.com>
2013-11-01 09:22:29 -0600

    Update UserVoice script and include actual features

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,19 +1,19 @@
 $(function() {
 
-  var handleHash = function(hash) {
-    console.log('handling: ' + hash);
-    if (hash.indexOf('feedback') === -1) {
-      UserVoice.hidePopupWidget();
-    } else {
-      UserVoice.showPopupWidget();
-    }
-  }
-
-  $(window).bind('hashchange', function() {
-    handleHash(window.location.hash);
-  });
-  
-  handleHash(window.location.hash);
+//  var handleHash = function(hash) {
+//    console.log('handling: ' + hash);
+//    if (hash.indexOf('feedback') === -1) {
+//      UserVoice.hidePopupWidget();
+//    } else {
+//      UserVoice.showPopupWidget();
+//    }
+//  }
+//
+//  $(window).bind('hashchange', function() {
+//    handleHash(window.location.hash);
+//  });
+//  
+//  handleHash(window.location.hash);
   
 /*
 width: 565, // width of slider panel
